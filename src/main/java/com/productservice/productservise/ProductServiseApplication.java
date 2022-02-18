c3adae28b7abec7c500715cbe70c2b1a24c50f03
avishtechnology01 <99983574+avishtechnology01@users.noreply.github.com>
2022-02-18 22:22:58 +0530

    initial commit by ashu

---
+++ src/main/java/com/productservice/productservise/ProductServiseApplication.java
@@ -0,0 +1,13 @@
+package com.productservice.productservise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductServiseApplication.class, args);
+	}
+
+}
